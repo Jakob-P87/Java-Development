5a0749dcf8534ba0e6dd79b6e5f21c533aecc03b
Jakob Persson <jakob.persson87@hotmail.com>
2019-10-31 12:14:15 +0100

    Removed folders

---
+++ Java-Programs/Assignment2/src/main/java/se/lexicon/jakob/App.java
@@ -0,0 +1,23 @@
+package se.lexicon.jakob;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println("Hello Arrays!");
+        System.out.println("Education for Arrays");
+
+        /*
+        int[] luckyNumber =  new int[3];
+        luckyNumber[0] = 8;
+        luckyNumber[1] = 8;
+        luckyNumber[2] = 8;
+        */
+
+        int[] luckyNumber = { 8, 17, 87};
+    }
+}
