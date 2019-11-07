6d5add0d74e37ef6ddeea372388b5713dd9f67bb
Jakob Persson <jakob.persson87@hotmail.com>
2019-11-07 14:51:39 +0100

    Added Todo class, and tested

---
+++ Java-Programs/TodoIt/src/main/java/se/lexicon/todoit/model/Todo.java
@@ -0,0 +1,50 @@
+package se.lexicon.todoit.model;
+
+public class Todo
+{
+    private final int todoId;
+    private String description;
+    private boolean done;
+    private Person assignee;
+
+    public Todo(final int todoId, String description)
+    {
+        this.todoId = todoId;
+        this.description = description;
+    }
+
+    public int getTodoId()
+    {
+        return todoId;
+    }
+
+    public String getDescription()
+    {
+        return description;
+    }
+
+    public boolean isDone()
+    {
+        return done;
+    }
+
+    public Person getAssignee()
+    {
+        return assignee;
+    }
+
+    public void setDescription(String description)
+    {
+        this.description = description;
+    }
+
+    public void setDone(boolean done)
+    {
+        this.done = done;
+    }
+
+    public void setAssignee(Person assignee)
+    {
+        this.assignee = assignee;
+    }
+}
